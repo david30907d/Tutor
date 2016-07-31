8312037e815c51a326a5ebf3e53ee3ef92152329
david <davidtnfsh@gmail.com>
2016-07-31 15:53:29 +0800

    優化counter變數

--- Ch2/crawler.py
+++ Ch2/crawler.py
@@ -1,27 +1,25 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 import requests, json, pyprind, sys, shutil
 from bs4 import BeautifulSoup
 json_arr = []
+
+def savePict(url, restaurant):
+    img = requests.get(url,stream=True)
+    with open(restaurant+'.jpg', 'wb') as f:
+        shutil.copyfileobj(img.raw, f)
 
 def dump(fileName):
 	with open(fileName, 'w', encoding='UTF-8') as f:
 		json.dump(json_arr, f)
 
-def savePict(url, resName):
-	img = requests.get(url, stream=True)
-	with open(resName+'.jpg', 'wb') as f:
-		shutil.copyfileobj(img.raw, f)
-
 def parsePage(url):
 	res = requests.get(url)
 	soup = BeautifulSoup(res.text)
+
 	aLen = len(soup.select("ul.deal16 li.box-shadow2px")) 
 	# ul.deal16 li.box-shadow2px是餐廳的tag
-	ProgreBar = pyprind.ProgBar(aLen, title = "台中 共 32 個餐廳類別要處理") 
-	aIndex = 1
+	ProgreBar = pyprind.ProgBar(aLen, title = "台中 共 {} 個餐廳類別要處理".format(aLen)) 
 
-	for i in soup.select('ul.deal16 li.box-shadow2px'):
+	for i, ProgIndex in zip( soup.select('ul.deal16 li.box-shadow2px'), range(1,aLen+1)):
 		product = i.select('h3.proname_3')[0].text
 		restaurant = i.select('h2.ref_name_2')[0].text	
 		imghref = i.select('img')[0]['src']
@@ -34,9 +32,9 @@ def parsePage(url):
 		json_arr.append(tmp)
 
 		savePict(imghref, restaurant)
-		ProgreBar.update(1,item_id = aIndex, force_flush=True)
-		aIndex+=1
-
+		#建立一個進度條物件
+		ProgreBar.update(1,item_id = ProgIndex, force_flush=True)
+		#item_id可以讓使用者追蹤到底執行到第幾個ID
 
 if __name__  ==  "__main__":
 	if len(sys.argv) < 2:
